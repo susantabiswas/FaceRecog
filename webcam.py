e27b39dbd91a5808f1242a49659def1a8fffdb4d
susantabiswas <susanta18@outlook.com>
2018-03-07 11:17:45 +0530

    added support for saving detected faces

--- webcam.py
+++ webcam.py
@@ -6,34 +6,38 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # WIDTH
 cap.set(4, 480)  # HEIGHT
 
-face_cascade = cv2.CascadeClassifier(
-    r'haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(r'haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(r'haarcascades/haarcascade_eye.xml')
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.flip(frame, 1, 0)
-
+    frame = cv2.flip(frame,1,0)
+    
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     print(len(faces))
-
+    
     # Display the resulting frame
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x-40, y-70), (x+w+20, y+h+40), (255, 0, 0), 2)
+        roi_color = frame[y-90:y+h+70, x-50:x+w+50]
         # save the detected face
         cv2.imwrite(save_loc, roi_color)
-
+        cv2.rectangle(frame, (x-40, y-70), (x+w+20, y+h+40), (255, 0, 0), 2)
+        
+        
         roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        
+        
+        
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
+            
+            
     cv2.imshow('frame', frame)
-
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
