db7b46dc3907500d70d36b436cb326f78bd1eb66
susantabiswas <susanta18@outlook.com>
2018-03-07 11:50:40 +0530

    text overlay support added

--- webcam.py
+++ webcam.py
@@ -2,11 +2,12 @@
 import cv2
 
 save_loc = r'saved_image/1.jpg'
-capture_obj = cv2.Videocapture_objture(0)
+capture_obj = cv2.VideoCapture(0)
 capture_obj.set(3, 640)  # WIDTH
 capture_obj.set(4, 480)  # HEIGHT
 
-face_cascade = cv2.CascadeClassifier(r'haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(
+    r'haarcascades/haarcascade_frontalface_default.xml')
 
 
 while(True):
@@ -19,7 +20,7 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # detect face
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    
+
     # Display the resulting frame
     for (x, y, w, h) in faces:
         # required region for the face
@@ -27,15 +28,20 @@ while(True):
         # save the detected face
         cv2.imwrite(save_loc, roi_color)
         # draw a rectangle bounding the face
-        cv2.rectangle(frame, (x-40, y-70), (x+w+20, y+h+40), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x-10, y-70), (x+w+20, y+h+40), (255, 0, 0), 2)
 
-    # display the frame with bounding rectangle    
+    # for putting text overlay on webcam feed
+    #text = 'Unknown face'
+    #font = cv2.FONT_HERSHEY_SIMPLEX
+    #cv2.putText(frame, text, (50, 50), font, 2, (255, 255, 0), 2)
+
+    # display the frame with bounding rectangle
     cv2.imshow('frame', frame)
 
     # close the webcam when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture_objture
+# release the capture_object
 capture_obj.release()
 cv2.destroyAllWindows()
