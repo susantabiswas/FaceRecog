7bac80a595e335a9865976541e0f698ce0101eb3
susantabiswas <susanta18@outlook.com>
2018-03-16 09:26:12 +0530

    Added support for multiple faces in single frame

--- webcam_utility.py
+++ webcam_utility.py
@@ -116,7 +116,7 @@ def detect_face_realtime(database, model, threshold=0.7):
 
                     if min_dist <= threshold and registered:
                         # for putting text overlay on webcam feed
-                        text = 'Welcome ' + identity
+                        text = identity
                         print('Welcome ' + identity + '!')
                     else:
                         text = 'Unknown user'
@@ -128,7 +128,7 @@ def detect_face_realtime(database, model, threshold=0.7):
             # draw a rectangle bounding the face
             cv2.rectangle(frame, (x-10, y-70),
                           (x+w+20, y+h+40), (15, 175, 61), 4)
-            cv2.putText(frame, text, (50, 50), font, 1.8, (158, 11, 40), 3)
+            cv2.putText(frame, text, (x - 15, y -80), font, 1.5, (158, 11, 40), 3)
 
         # display the frame with bounding rectangle
         cv2.imshow('frame', frame)
