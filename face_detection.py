3bb982d5457e48da7fdac93a8293c7bfccebf3a2
Susanta <susantasky@gmail.com>
2021-01-04 20:55:24 +0530

    face detection class added

--- face_detection.py
+++ face_detection.py
@@ -1,3 +1,11 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+'''Description: Class for face detection. Uses a CNN 
+based neural network to get the bounding box coordinates 
+for a human face'''
+# ===================================================
 from exceptions import ModelFileMissing
 import cv2
 import os  
@@ -15,10 +23,46 @@ class FaceDetector:
             raise ModelFileMissing
         try:
             # load the model
-            self.face_detector = cv2.dnn.readFromTensorflow(model_path,
+            self.face_detector = cv2.dnn.readNetFromTensorflow(model_path,
                                                         config_path)
         except Exception as e:
             raise e
 
-    def detect_face(self, image):
-        pass
+    def detect_face(self, image, conf_threshold: float=0.7):
+        # To prevent modification of orig img
+        image = image.copy()
+
+        # Run the face detection model on the image to get 
+        # bounding box coordinates
+        # create input image blob
+        img_blob = cv2.dnn.blobFromImage(image, 1.0, \
+                        (300, 300), [104, 117, 123], False, False)
+        # Feed the input blob to NN and get the output layer predictions
+        self.face_detector.setInput(img_blob)
+        detections = self.face_detector.forward()
+
+        # Scale the bbox coordinates
+        height, width = image.shape[:2]
+
+        # Bounding box coordinates of faces in image
+        bboxes = []
+        for idx in range(detections.shape[2]):
+            conf = detections[0, 0, idx, 2]
+            if conf >= conf_threshold:
+                x1 = int(detections[0, 0, idx, 3] * width)
+                y1 = int(detections[0, 0, idx, 4] * height)
+                x2 = int(detections[0, 0, idx, 5] * width)
+                y2 = int(detections[0, 0, idx, 6] * height)
+
+                bboxes.append([x1, y1, x2, y2])
+
+        return bboxes
+
+
+
+if __name__ == "__main__":
+    # Sample Usage
+    ob = FaceDetector(model_loc='models')
+    img = cv2.imread('data/sample/1.jpg')
+    bbox = ob.detect_face(img)
+    print(bbox)
