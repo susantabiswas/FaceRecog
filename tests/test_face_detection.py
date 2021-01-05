db2469a3d190f2141933a401bd6b1c3b0948a55a
Susanta <susantasky@gmail.com>
2021-01-05 21:34:36 +0530

    pytest config changes

--- tests/test_face_detection.py
+++ tests/test_face_detection.py
@@ -1,19 +1,19 @@
 import pytest
 from face_recog.exceptions import ModelFileMissing
 from face_recog.face_detection import FaceDetector
+import cv2 
 
-def test_correct_model_path(benchmark):
+def test_correct_model_path():
         """
             Test object init with the correct model path 
         """
         ob = None
         model_loc='./models'
         try:
-            ob = benchmark(FaceDetector(model_loc=model_loc))
+            ob = FaceDetector(model_loc=model_loc)
         except Exception: 
             pass
         finally:
-            print('*****************************',ob)
             assert isinstance(ob, FaceDetector)
 
 def test_incorrect_model_path():
@@ -26,4 +26,7 @@ def test_incorrect_model_path():
         ob = FaceDetector(model_loc=inccorrect_model_loc)
 
 
-test_correct_model_path()
+def test_detect_face(img_data):
+    model_loc='./models'
+    ob = FaceDetector(model_loc=model_loc)
+    assert [[348, 76, 407, 166]] == ob.detect_face(img_data)
