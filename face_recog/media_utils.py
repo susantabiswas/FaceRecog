af68b068abe3e501814770dea269013bd274d645
Susanta <susantasky@gmail.com>
2021-01-08 22:48:06 +0530

    helper methods related to videos and images

---
+++ face_recog/media_utils.py
@@ -0,0 +1,22 @@
+import cv2
+from face_recog.validators import is_valid_img
+from face_recog.exceptions import InvalidImage
+import dlib 
+
+def convert_to_rgb(self, image):
+    if not is_valid_img(image):
+        raise InvalidImage
+    
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+def convert_to_dlib_rectangle(self, bbox):
+    return dlib.rectangle(bbox[0], bbox[1], bbox[2], bbox[3])
+
+
+def load_image_path(self, img_path):
+    try:
+        img = cv2.imread(img_path)
+        return img
+    except Exception as exc:
+        raise exc
