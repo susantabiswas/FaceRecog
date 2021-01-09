77df05ee0e085cd920e22debfe5932b38aac5e40
Susanta <susantasky@gmail.com>
2021-01-09 16:33:24 +0530

    added image operation related functions

--- face_recog/media_utils.py
+++ face_recog/media_utils.py
@@ -3,18 +3,19 @@ from face_recog.validators import is_valid_img
 from face_recog.exceptions import InvalidImage
 import dlib 
 
-def convert_to_rgb(self, image):
+def convert_to_rgb(image):
     if not is_valid_img(image):
         raise InvalidImage
     
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-def convert_to_dlib_rectangle(self, bbox):
-    return dlib.rectangle(bbox[0], bbox[1], bbox[2], bbox[3])
+def convert_to_dlib_rectangle(bbox):
+    return dlib.rectangle(bbox[0], bbox[1],
+                         bbox[2], bbox[3])
 
 
-def load_image_path(self, img_path):
+def load_image_path(img_path):
     try:
         img = cv2.imread(img_path)
         return img
