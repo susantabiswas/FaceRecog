b5d190ac86de0fd5717737876a5890abf81eaa79
Susanta <susantasky@gmail.com>
2021-01-10 20:02:06 +0530

    reverted data deserialization

--- face_recog/json_persistent_storage.py
+++ face_recog/json_persistent_storage.py
@@ -19,7 +19,7 @@ from typing import Dict
 class JSONStorage(PersistentStorage):
     def __init__(self, db_loc:str='./data/facial_data_db.json'):
         self.db_loc = db_loc
-
+        
     def add_data(self, face_data:Dict):
         data = []
         # check if the db exists, otherwise create one
@@ -38,6 +38,7 @@ class JSONStorage(PersistentStorage):
 
             with open(self.db_loc, 'w') as f:
                 json.dump(data, f)
+            print('[INFO] Data saved to DB...')
         except Exception as exc:
             raise exc
 
@@ -50,17 +51,23 @@ class JSONStorage(PersistentStorage):
             # load the existing data
             with open(self.db_loc, 'r') as f:
                 data = json.load(f)
+                # deserialize the dict data and convert back to dict
                 return data
         except Exception as exc:
             raise exc
 
 
+
 if __name__ == "__main__":
+    import numpy as np
     ob = JSONStorage(db_loc='data/test_facial_data.json')
-    ob.add_data(3)
+    # Save data
+    face_data = {'name': 'test2', 
+            'encoding': (-3.4, 0.3, -.823, 1)}
+    ob.add_data(face_data=face_data)
     print(ob.get_all_data())
 
-    ob.add_data(4)
+    ob.add_data(face_data=face_data)
     print(ob.get_all_data())
 
     # remove the test file
