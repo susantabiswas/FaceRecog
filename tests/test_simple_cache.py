78321d3dcca62e0d4f9e9bff995dec28bc1faa24
Susanta <susantasky@gmail.com>
2021-01-10 21:57:06 +0530

    test cases fixed

--- tests/test_simple_cache.py
+++ tests/test_simple_cache.py
@@ -2,31 +2,49 @@ from face_recog.exceptions import InvalidCacheInitializationData
 import pytest
 from face_recog.simple_cache import SimpleCache
 
-def test_correct_initializer():
+def test_correct_initializer(correct_cache_init_data):
     """ Check if a simple cache can be initialized
     with expected data structure i.e., list"""
-    ob = SimpleCache([1, 2])
-    assert ob.get_all_data == [1, 2]
+    for test_case in correct_cache_init_data:
+        try:
+            ob = SimpleCache(test_case)
+        except Exception:
+            print('Failed', test_case)
+            pytest.fail()
 
-def test_correct_initializer():
+def test_incorrect_initializer(incorrect_cache_init_data):
     """ Check if a simple cache can be initialized
-    with an incoorect data structure"""
-    with pytest.raises(InvalidCacheInitializationData):
-        ob = SimpleCache("1, 2")
+    with an incorect data structure"""
+    for test_case in incorrect_cache_init_data:
+        with pytest.raises(InvalidCacheInitializationData):
+            print('*******TEst case******',test_case)
+            ob = SimpleCache(test_case)
     
-def test_unique_add_data():
+def test_unique_add_data(simple_cache_init_data, simple_cache_data2):
     """ Check if duplicates get added or not"""
-    ob = SimpleCache([1, 2])
-    ob.add_data(1)
-    assert ob.get_all_data() == [1, 2]
+    ob = SimpleCache(simple_cache_init_data)
+    ob.add_data(simple_cache_data2)
+    assert sorted(ob.get_all_data(), key=lambda x: x['name']) == \
+        sorted([{'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test2'}, 
+                                {'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test1'}],
+                                key=lambda x: x['name'])
  
-def test_add_data():
-    """ Check if a data item gets added"""
-    ob = SimpleCache([1, 2])
-    ob.add_data(11)
-    assert ob.get_all_data() == [1, 2, 11]
 
-def test_get_all_data():
+def test_add_data(simple_cache_init_data, simple_cache_data3):
+    """ Check if a data item gets added"""
+    ob = SimpleCache(simple_cache_init_data)
+    ob.add_data(simple_cache_data3)
+    assert sorted(ob.get_all_data(), key=lambda x: x['name']) \
+            == sorted([{'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test1'},
+                        {'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test3'}, 
+                        {'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test2'}],
+                        key=lambda x: x['name'])
+
+
+def test_get_all_data(simple_cache_init_data):
     """ Check the data from cache"""
-    ob = SimpleCache([1, 2])
-    assert ob.get_all_data() == [1, 2]
+    ob = SimpleCache(simple_cache_init_data)
+    assert sorted(ob.get_all_data(), key=lambda x: x['name']) == \
+        sorted([{'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test2'},
+                {'encoding': (-3.4, 0.3, -0.823, 1), 'name': 'test1'}],
+                key=lambda x: x['name'])
