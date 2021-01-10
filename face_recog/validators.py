a7dceec58d5ca318c02135ec9aac8192e40f65db
Susanta <susantasky@gmail.com>
2021-01-10 16:52:24 +0530

    path checker added

--- face_recog/validators.py
+++ face_recog/validators.py
@@ -1,3 +1,11 @@
+import os
+
+
 def is_valid_img(image):
     return not (len(image.shape) != 3 
                 or image.shape[-1] != 3)
+
+def path_exists(path=None):
+    if path and os.path.exists(path):
+        return True
+    return False
