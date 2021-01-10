5a3c0f29e6751f78f2ee3a463713a2b93487fbbd
Susanta <susantasky@gmail.com>
2021-01-10 16:49:59 +0530

    data handler exceptions

--- face_recog/exceptions.py
+++ face_recog/exceptions.py
@@ -41,3 +41,20 @@ class InvalidImage(Exception):
     def __init__(self) -> None:
         self.message = "Invalid Image!!"
 
+class DatabaseFileNotFound(Exception):
+    """Raised when the persistent storage databse file
+    doesn't exists at the given location.
+    Attributes:
+        message: (str) Exception message
+    """
+    def __init__(self) -> None:
+        self.message = "Database file not found!!"
+
+class InvalidCacheInitializationData(Exception):
+    """Raised when a data structure other than
+    a list is supplied for cache initialization.
+    Attributes:
+        message: (str) Exception message
+    """
+    def __init__(self) -> None:
+        self.message = "Invalid data structure. Please suppply a list!!"
