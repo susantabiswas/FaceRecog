3cba3fb4bc885f37a348456cb5e4e420ffc55651
Susanta <susantasky@gmail.com>
2021-01-10 16:51:32 +0530

    abstract data handler classes

--- face_recog/in_memory_cache.py
+++ face_recog/in_memory_cache.py
@@ -5,3 +5,13 @@
 '''Description: For handling data management in memory
  cache'''
 # ===================================================
+from abc import ABC, abstractmethod
+
+class InMemoryCache(ABC):
+    @abstractmethod
+    def add_data(self):
+        pass
+
+    @abstractmethod
+    def get_all_data(self):
+        pass
--- face_recog/persistent_storage.py
+++ face_recog/persistent_storage.py
@@ -5,3 +5,14 @@
 '''Description: Base class to handle persistent data 
 storage'''
 # ===================================================
+from abc import ABC, abstractclassmethod, abstractmethod
+
+class PersistentStorage(ABC):
+    @abstractmethod
+    def add_data(self):
+        pass
+
+    @abstractmethod
+    def get_all_data(self):
+        pass 
+    
