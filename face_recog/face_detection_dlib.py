afc391e47d87d78297a35272d3a113b8976a398c
Susanta <susantasky@gmail.com>
2021-01-16 20:24:45 +0530

    model file check added

--- face_recog/face_detection_dlib.py
+++ face_recog/face_detection_dlib.py
@@ -13,7 +13,7 @@ Ref: http://dlib.net/cnn_face_detector.py.html
 
 from face_recog.media_utils import convert_to_rgb, draw_bounding_box
 from face_recog.validators import is_valid_img
-from face_recog.exceptions import InvalidImage
+from face_recog.exceptions import InvalidImage, ModelFileMissing
 import os  
 from typing import List
 from face_recog.face_detector import FaceDetector
@@ -32,6 +32,9 @@ class FaceDetectorDlib(FaceDetector):
                 self.face_detector = dlib.get_frontal_face_detector()
             else:
                 cnn_model_path = os.path.join(model_loc, FaceDetectorDlib.cnn_model_filename) 
+                if not os.path.exists(cnn_model_path):
+                    raise ModelFileMissing
+                # MMOD model
                 self.face_detector = dlib.cnn_face_detection_model_v1(cnn_model_path)
             self.model_type = model_type
             print('[INFO] dlib: {} face detector loaded...'.format(self.model_type))
@@ -40,8 +43,6 @@ class FaceDetectorDlib(FaceDetector):
 
 
     def detect_faces(self, image, num_upscaling:int=1)->List[List[int]]:
-        if image is None:
-            return []
         if not is_valid_img(image):
             raise InvalidImage
         
@@ -77,26 +78,26 @@ if __name__ == "__main__":
     
     # Sample Usage
     ob = FaceDetectorDlib(model_type='hog')
-    img = cv2.imread('data/sample/1.jpg')
+    img = cv2.imread('data/sample/2.jpg')
     print(img.shape)
     bbox = ob.detect_faces(convert_to_rgb(img))
     print(bbox)
     # draw_bounding_box(img, bbox)
 
-    small_frame = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-    print(small_frame.shape)
-    bbox1 = ob.detect_faces(convert_to_rgb(small_frame))
-    print('smaller',bbox1)
-    draw_bounding_box(small_frame, bbox1[0], color=(255,0,255))
-    cv2.imshow('Test',small_frame)
-    cv2.waitKey(0)
+    # small_frame = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    # print(small_frame.shape)
+    # bbox1 = ob.detect_faces(convert_to_rgb(small_frame))
+    # print('smaller',bbox1)
+    # draw_bounding_box(small_frame, bbox1[0], color=(255,0,255))
+    # cv2.imshow('Test',small_frame)
+    # cv2.waitKey(0)
 
-    # rescale
-    import numpy as np
-    bbox1 = 2*np.array(bbox1)
-    print('rescaled',bbox1)
-    draw_bounding_box(img, bbox[0])
-    draw_bounding_box(img, bbox1[0], color=(255,255,0))
+    # # rescale
+    # import numpy as np
+    # bbox1 = 2*np.array(bbox1)
+    # print('rescaled',bbox1)
+    # draw_bounding_box(img, bbox[0])
+    # draw_bounding_box(img, bbox1[0], color=(255,255,0))
         
-    cv2.imshow('Test',img)
-    cv2.waitKey(0)
+    # cv2.imshow('Test',img)
+    # cv2.waitKey(0)
