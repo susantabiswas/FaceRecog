b8b341029f308e9af5ea76f552cfd2c992ff2d46
Susanta <susantasky@gmail.com>
2021-01-17 22:04:39 +0530

    documentation added

--- face_recog/logger.py
+++ face_recog/logger.py
@@ -1,27 +1,72 @@
 import logging
 import sys
 import traceback
+from typing import List
 
 class LoggerFactory:
     def create_formatter(self, format_pattern:str):
+        """Creates a logger formatter with user defined/deafult format.
+
+        Args:
+            format_pattern (str, optional): Logger message format. Defaults to None.
+        Returns:
+            logger formatter
+        """
         format_pattern = format_pattern or \
                 "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s"
         return logging.Formatter(format_pattern)
 
-    def get_console_handler(self, formatter, level=logging.INFO, stream=sys.stdout):
+    def get_console_handler(self, formatter, 
+        level=logging.INFO, stream=sys.stdout):
+        """Returns a stream handler for logger
+
+        Args:
+            formatter : logger formatter object
+            level (optional): Logger level. Defaults to logging.INFO.
+            stream (stream, optional): Stream type. E.g: STDERR, STDOUT
+                Defaults to sys.stdout.
+
+        Returns:
+            logger stream handler
+        """
         # create a stream handler, it can be for stdout or stderr
         console_handler = logging.StreamHandler(stream)
         console_handler.setFormatter(formatter)
         console_handler.setLevel(level)
         return console_handler
         
-    def get_file_handler(self, formatter, level=logging.INFO, file_path:str='data/app.log'):
+    def get_file_handler(self, formatter, 
+            level=logging.INFO, file_path:str='data/app.log'):
+        """Returns a file handler for logger
+
+        Args:
+            formatter : logger formatter object
+            level (optional): Logger level. Defaults to logging.INFO.
+            file_path (str, optional): Path where the log file should be saved. 
+                Defaults to 'data/app.log'.
+
+        Returns:
+            logger file handler
+        """
         file_handler = logging.FileHandler(filename=file_path) 
         file_handler.setFormatter(formatter)
         file_handler.setLevel(level)
         return file_handler
 
-    def get_logger(self, logger_name, level=logging.DEBUG, format_pattern:str=None, file_path:str='data/app.log'):
+    def get_logger(self, logger_name, level=logging.DEBUG, 
+                format_pattern:str=None, file_path:str='data/app.log'):
+        """Creates a logger with pre-defined settings
+
+        Args:
+            logger_name (str): Name of logger
+            level (optional): Logger level. Defaults to logging.DEBUG.
+            format_pattern (str, optional): Logger message format. Defaults to None.
+            file_path (str, optional): Path where the log file should be saved. 
+                Defaults to 'data/app.log'.
+
+        Returns:
+            logger
+        """
         # Creates the default logger
         logger = logging.getLogger(logger_name)
         formatter = self.create_formatter(format_pattern=format_pattern)
@@ -37,7 +82,19 @@ class LoggerFactory:
         logger.propagate = False
         return logger
 
-    def create_logger(self, logger_name, handlers, propagate_error:bool=False):
+    def create_logger(self, logger_name:str, handlers:List, 
+                propagate_error:bool=False):
+        """Creates a custom logger.
+
+        Args:
+            logger_name (str): Name of logger
+            handlers (List) : Logger handlers. E.g: Stream handlers like file handler, console handler etc.
+            propagate_error (bool, optional): Whether the errors should be propagated to the parent.
+                Defaults to False.
+
+        Returns:
+            logger 
+        """
         logger = logging.getLogger(logger_name)
         # Add all the stream handlers
         for handler in handlers:
@@ -46,7 +103,19 @@ class LoggerFactory:
         return logger
 
     def uncaught_exception_hook(self, logger, type, value, tb):
-        
+        """Handles uncaught exceptions and saves the details
+        using the logger. So if the logger has console and file
+        stream handlers, then the uncaught exception will be sent there.    
+
+        Args:
+            logger (logger): Python native logger
+            type (Exception type): 
+            value (Exception value): Exception message
+            tb (traceback): 
+
+        Raises:
+            The uncaught exception
+        """
         # Returns a list of string sentences
         tb_message = traceback.extract_tb(tb).format()
         tb_message = '\n'.join(tb_message)
@@ -56,6 +125,8 @@ class LoggerFactory:
         # Raise so that it can be captured by 3rd party tools
         raise type(value)
 
-# sys.excepthook = LoggerFactory().my_exception_hook
-sys.excepthook = LoggerFactory().uncaught_exception_hook
-10 + "0"
+
+# sys.excepthook = LoggerFactory().uncaught_exception_hook
+# ValueError
+
+
